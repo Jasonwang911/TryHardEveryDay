4f6d6d1b456fb9316f63e3aa53e80cacf248e00a
Jasonwang911 <396689144@qq.com>
2019-08-08 00:05:02 +0800

    vue-ssr实现

---
+++ VueSSR/step2/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <div>
+   <div>我是App.vue, {{msg}}</div>
+   <bar />
+   <foo />
+ </div>
+</template>
+
+<script>
+import Bar from "./components/Bar";
+import Foo from "./components/Foo";
+
+export default {
+  data() {
+    return {
+      msg: "hello vue-ssr"
+    };
+  },
+  components: {
+    Bar,
+    Foo
+  }
+};
+</script>
+
+<style scoped>
+</style>
