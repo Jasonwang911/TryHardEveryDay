760d2f3ff7ab00126028f0d3aebdacc523c717fd
Jasonwang911 <396689144@qq.com>
2021-04-29 19:33:58 +0800

    算法

--- 数据结构和算法/js/test.js
+++ 数据结构和算法/js/test.js
@@ -1,11 +1,35 @@
+// var reverseWords = function (s) {
+//   return s.split(' ').map(item => {
+//     return item.split('').reverse().join('')
+//   }).join(' ')
+// }
+
 var reverseWords = function (s) {
-  let wordArr = s.split(' ')
-  console.log(wordArr)
-  for (let i = 0; i <= wordArr.length - 1; i++) {
-    wordArr[i] = wordArr[i].split('').reverse().join('')
-    console.log(wordArr[i])
-  }
-  return wordArr.join(' ')
+ let res = ''
+ for(let i = s.length-1; i >=0; i --) {
+   res += s[i]
+ }
+ return res.split(' ').reverse().join(' ')
 }
 
-console.log('输出结果===>', reverseWords("Let's take LeetCode contest"))
+// console.log('输出结果===>', reverseWords("Let's take LeetCode contest"))
+
+let s =  "Let's take LeetCode contest"
+let res = s.split('').reverse().join('').split(' ').reverse().join(' ')
+
+
+var twoSum = function(nums, target) {
+  let res = []
+  for(let i = 0; i < nums.length; i ++) {
+      for(let j = 1; j < nums.length; j ++) {
+        console.log(`${nums[i]}---${nums[j]}, ---${i}---${j}`)
+          if(nums[i] + nums[j] === target) {
+            res = [i,j]
+          }
+      }
+  }
+  return res
+};
+
+let sss = twoSum([2,5,5,11],10)
+console.log('=====>', sss)
