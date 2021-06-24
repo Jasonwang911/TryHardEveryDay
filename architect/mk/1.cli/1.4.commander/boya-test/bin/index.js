cb6b3a10ea5921d7dffd1bd29389a661985a5b4f
jasonwang <396689144@qq.com>
2021-06-25 00:27:31 +0800

    command

--- architect/mk/1.cli/1.4.commander/boya-test/bin/index.js
+++ architect/mk/1.cli/1.4.commander/boya-test/bin/index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 
+const { description } = require('commander')
 const commander = require('commander')
 const pkg = require('../package.json')
 
@@ -11,15 +12,60 @@ const program = new commander.Command()
 // 使用构造者模式 进行连点操作
 program
   .name(Object.keys(pkg.bin)[0])
-  .usage('[commander] <options>')
+  .usage('<commander> [options]')
   .version(pkg.version)
   .option('-d, --debug', '是否开启调试模式', false)
   .option('-e, --envName <envName>', '获取环境变量名称')
-program.command('clone <source> [destination]')
-  .description('clone a repository')
-  .action( () => {
-    console.log('do clone')
+const clone = program.command('clone <source> [destination]')
+clone
+  .description('描述')
+  .option('-f, --force', '强制拷贝')
+  .action((source, destination, cmdObj ) => {
+    console.log('do clone', source, destination, cmdObj.force)
   })
+  // 注册子命令
+const service = new commander.Command('service')
+service
+  .command('start [port]')
+  .description('start service at some port')
+  .action((port) => {
+    console.log(`service is running at ${port}`)
+  })
+program.addCommand(service)  
+
+// program
+//   .arguments('<cmd> [option]')
+//   .description('test command', {
+//     cmd: 'cmd to run',
+//     option: 'option for command'
+//   })
+//   .action((cmd, option) => {
+//     console.log(cmd, option)
+//   })
+
+// 高级定制1： 自定义help信息
+// program.helpInformation = () => {return ''}
+// program.on('--help', () => {
+//   console.log('your help information')
+// })
+
+// 高级定制2： 实现debug模式, 并且早于命令执行之前
+program.on('option:debug', () => {
+  if(program.opts().debug) {
+    process.env.LOG_LEVEL = 'verbose'
+  }
+  console.log(process.env.LOG_LEVEL)
+})
+
+// 高级定制3： 对未知命令监听
+program.on('command:*', (obj) => {
+  console.log('未知命令：',  obj )
+  const avaliableCommands = program.commands.map(item => item.name()).join(',')
+  console.log(avaliableCommands)
+  console.log(program.commands[0].name() )
+})
+
+program
   .parse(process.argv)
 
 // console.log(program.opts())
--- architect/mk/1.cli/commander.md
+++ architect/mk/1.cli/commander.md
@@ -6,6 +6,42 @@
 
 2. 注册命令
 - command 注册命令
-program.command() 会得到一个返回值，这个返回值是一个新的命令对象，并不是program对象
+program.command() 会得到一个返回值，这个返回值是一个新的命令对象，并不是program对象     
+options参数
+- executableFile 可执行文件(实现多脚手架之间的串行使用)
+- isDefault: true  设置默认命令
+- hidden: true    隐藏命令
 
-- addCommand 注册命令
+- addCommand 注册子命令
+
+3. arguments() 监听所有的命令输入,并强制统一约束所传参数
+
+### 高级定制
+1. 自定义help信息
+program.helpInformation()
+```
+program.helpInformation = () => {return ''}
+program.on('--help', () => {
+  console.log('your help information')
+})
+```
+
+2. 实现debug模式, 并且早于命令执行之前
+```
+program.on('option:debug', () => {
+  if(program.opts().debug) {
+    process.env.LOG_LEVEL = 'verbose'
+  }
+  console.log(process.env.LOG_LEVEL)
+})
+```
+
+3. 对未知命令监听
+```
+program.on('command:*', (obj) => {
+  console.log('未知命令：',  obj )
+  const avaliableCommands = program.commands.map(item => item.name()).join(',')
+  console.log(avaliableCommands)
+  console.log(program.commands[0].name() )
+})
+```
