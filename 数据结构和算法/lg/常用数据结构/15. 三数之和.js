ae09a9a84be513acf2e9f53e28bb5cea652e6ec7
jasonwang <396689144@qq.com>
2021-07-13 01:54:35 +0800

    leetcode

--- 数据结构和算法/lg/常用数据结构/15. 三数之和.js
+++ 数据结构和算法/lg/常用数据结构/15. 三数之和.js
@@ -17,7 +17,7 @@
 // 输入：nums = [0]
 // 输出：[]
 
-const nums = [-1,0,1,2,-1,-4]
+const nums = [-2,0,3,-1,4,0,3,4,1,1,1,-3,-5,4,0]
 
 var threeSum = function(nums) {
   let result = []
@@ -25,7 +25,7 @@ var threeSum = function(nums) {
   nums.sort((a, b) => a-b)
   // 2. 遍历数组，从0遍历到length-2
   for(let i = 0; i < nums.length - 2; i++) {
-     // 3. 如果当前的数字等于前一个数字，则跳过当前这个数
+     // 3. 如果当前的数字等于前一个数字，则跳过当前这个数(为了去重)
     if(i === 0 || nums[i] !== nums[i-1]) {
       // 4. 如果数字不同，则设置start = i +1; end = length -1 查看 i start end 三个数的和比0大还是小，如果比0小 start ++; 如果比0大 end--; 如果等于0； 把这三个数加入到结果里
       let start = i + 1
@@ -35,6 +35,7 @@ var threeSum = function(nums) {
           result.push([nums[i], nums[start], nums[end]] )
           start ++
           end --
+          // 去重
           while(start<end && nums[start] === nums[start-1]) {
             start++
           }
